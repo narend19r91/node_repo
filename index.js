e19e80aa690212b20e103586109d0ad7006e1bc6
user.name <narend19r91@gmail.com>
2021-01-25 20:50:17 +0530

    Callback-Function-example

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
+const rectangle = require('./rectangle');
 var rect=require('./rectangle');
 
 function solveRect(l,b){
 
     console.log("solving for the rectangle l="+l+"and b="+b);
 
-    if(l<=0 && b<=0){
-       console.log("Rectangle dimensions should be greater l="+l+"and b="+b);
-    }else{
-        console.log("Area of the rectangle is="+rect.area(l,b));
-        console.log("Perimeter of the Rectangle is ="+rect.perimeter(l,b));
-    }
+
+    //using call back funtion implementation
+
+    rect(l,b,(error,rectangle)=>{
+        if(error){
+            console.log("ERROR:"+error);
+        }else{
+            console.log("The area of the rectangle of dimenasion l="+"and b="+b+" is "+rectangle.area());
+            console.log("The Perimeter of the rectangle of dimenasion l="+"and b="+b+" is "+rectangle.perimeter());
+        }
+    });
+
+    console.log("This statement is after the call to rect()");
+    //node module implementaion
+    // if(l<=0 && b<=0){
+    //    console.log("Rectangle dimensions should be greater l="+l+"and b="+b);
+    // }else{
+    //     console.log("Area of the rectangle is="+rect.area(l,b));
+    //     console.log("Perimeter of the Rectangle is ="+rect.perimeter(l,b));
+    // }
 }
 
 solveRect(7,8);
 solveRect(10,20);
+solveRect(0,-3);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,28 @@
-exports.perimeter=(x,y)=>(2*(x+y)),
-exports.area=(x,y)=>(x*y)
+//using callback function 
+
+module.exports=(x,y,callback)=>{
+    if(x<=0 || y<=0){
+            setTimeout(()=>
+            callback(new Error("Rectangle dimensions show are less then zero value of l"+x +"and b"+y),
+            null)
+            ,2000);
+    }else{
+            
+        setTimeout(()=>callback(null,
+            {
+                // perimeter:(x,y)=>(2*(x+y)),
+                // area:(x,y)=>(x*y)
+                perimeter:()=>(2*(x+y)),
+                area:()=>(x*y)
+
+            }),
+            2000);
+    }
+
+}
+
+
+
+//using node module way
+// exports.perimeter=(x,y)=>(2*(x+y)),
+// exports.area=(x,y)=>(x*y)
